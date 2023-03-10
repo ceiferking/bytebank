ec213b4429c05d392e37552b8f26830be7c114a4
Cristian Weber <47835197+ceiferking@users.noreply.github.com>
2023-03-09 23:10:13 -0300

    Update Conta.java

--- src/bytebank/Conta.java
+++ src/bytebank/Conta.java
@@ -1,36 +1,74 @@
 package bytebank;
 
 public class Conta {
-	double saldo;
-	int agencia;
-	int numero;
-	String titular;
+	private double saldo;
+	private int agencia;
+	private int numero;
+	private cliente titular;
 	
-	public void deposita(double valor) {
-		this.saldo  = this.saldo + valor; 
+	//permite consultar o saldo;
+	public double getSaldo(){
+		return saldo;
 	}
 	
+	//Função para realizar deposito//
+	public void deposita(double valor) {
+		this.saldo = this.saldo + valor;
+	}
+	
+	//função para realizar saques//
 	public void saca(double valor) {
-		if(saldo - valor != 0 ) {
+		if (saldo - valor != 0) {
 			System.out.println("não tem saldo suficiente");
 			System.out.println("seu saldo atual é de: " + saldo);
 		}
-		if(saldo - valor >= 0) {
+		if (saldo - valor >= 0) {
 			this.saldo = this.saldo - valor;
 			System.out.println("seu saldo atual é de: " + saldo);
 		}
 	}
 	
+	//função para realizar transferencias entre contas//
 	public void transfere(double valor, Conta destino) {
-		if(saldo - valor != 0 ) {
+		if (saldo - valor != 0) {
 			System.out.println("não tem saldo suficiente");
 			System.out.println("seu saldo atual é de: " + saldo);
 		}
-		if(saldo - valor >= 0) {
+		if (saldo - valor >= 0) {
 			this.saldo = this.saldo - valor;
 			destino.deposita(valor);
 			System.out.println("Transferencia realissada com sucesso!");
 			System.out.println("seu saldo atual é de: " + saldo);
 		}
 	}
+	
+	public int getNumero() {
+		return this.numero;
+	}
+	
+	public void setNumero(int novoNumero) {
+		this.numero = novoNumero;
+	}
+	
+	public int getAgencia() {
+		return this.agencia;
+	}
+	
+	public void setAgencia(int agencia) {
+		this.agencia = agencia;
+	}
+	
+	public void setTitular(cliente titular) {
+		this.titular = titular;
+	}
+	
+	public cliente getTitular() {
+		return titular;
+	}
+
+	public void setSaldo(double saldo) {
+		this.saldo = saldo;
+	}
+	
+
 }
